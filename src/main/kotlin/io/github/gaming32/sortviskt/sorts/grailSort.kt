397c8c455c28085d27f62d391248b3ab7bbdfce9
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-08-31 17:39:11 -0500

    Include broken OOP Grail Sort

--- src/main/kotlin/io/github/gaming32/sortviskt/sorts/grailSort.kt
+++ src/main/kotlin/io/github/gaming32/sortviskt/sorts/grailSort.kt
@@ -2,11 +2,29 @@ package io.github.gaming32.sortviskt.sorts
 
 import io.github.gaming32.sortviskt.AbstractSort
 import io.github.gaming32.sortviskt.VisualList
+import io.github.holygrailsortproject.rewrittengrailsort.GRAIL_STATIC_EXT_BUFFER_LEN
 import io.github.holygrailsortproject.rewrittengrailsort.lazyStableSort
 
 class GrailSort : AbstractSort("Grail Sort") {
     override fun run(list: VisualList) {
         io.github.holygrailsortproject.rewrittengrailsort.GrailSort<Int>().commonSort(list, null)
+    }
+}
+
+class GrailSortStaticOop : AbstractSort("Grail Sort (Static OOP)") {
+    override fun run(list: VisualList) {
+        io.github.holygrailsortproject.rewrittengrailsort.GrailSort<Int>()
+            .commonSort(list, arrayOfNulls(GRAIL_STATIC_EXT_BUFFER_LEN))
+    }
+}
+
+class GrailSortDynamicOop : AbstractSort("Grail Sort (Dynamic OOP)") {
+    override fun run(list: VisualList) {
+        var bufferLen = 1
+        while (bufferLen * bufferLen < list.size) {
+            bufferLen *= 2
+        }
+        io.github.holygrailsortproject.rewrittengrailsort.GrailSort<Int>().commonSort(list, arrayOfNulls(bufferLen))
     }
 }
 
